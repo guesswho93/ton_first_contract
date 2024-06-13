a1b3148dc49ddaca943a8ad9abfab53fab525ca0
Artem Kropotov <satnightfever@mail.ru>
2024-06-13 13:32:35 +0300

    Initial commit

---
+++ wrappers/Main.ts
@@ -0,0 +1,64 @@
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, toNano } from '@ton/core';
+
+export type MainConfig = {
+    admin: Address;
+};
+
+export function mainConfigToCell(config: MainConfig): Cell {
+    return beginCell()
+        .storeAddress(config.admin)
+        .endCell();
+}
+
+export class Main implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new Main(address);
+    }
+
+    static createFromConfig(config: MainConfig, code: Cell, workchain = 0) {
+        const data = mainConfigToCell(config);
+        const init = { code, data };
+        return new Main(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendFunds(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0xa4d8086f, 32)
+                .endCell(),
+        });
+    }
+
+    async sendWithdraw(provider: ContractProvider, via: Sender) {
+        await provider.internal(via, {
+            value:toNano('0.38'), 
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0x217e5898, 32)
+                .endCell()
+        });
+    }
+
+
+    async getBalance(provider: ContractProvider): Promise<number> {
+        const res = (await provider.get('get_contract_balance', [])).stack;
+        return res.readNumber();
+    }
+
+    async getAdmin(provider: ContractProvider): Promise<Address> {
+        const res = (await provider.get('get_admin_address', [])).stack;
+        return res.readAddress();
+    }
+}
